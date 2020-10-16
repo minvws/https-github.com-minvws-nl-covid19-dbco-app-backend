df53434e9b0efb463e1c891389e605fd357f757e
Ivo Jansch <ivo@egeniq.com>
2020-10-16 17:10:55 +0200

    Refactor case level for healthauthority backend

--- api/src/healthauthority/bootstrap/services.php
+++ api/src/healthauthority/bootstrap/services.php
@@ -1,16 +1,13 @@
 <?php
 declare(strict_types=1);
 
-use App\Application\Services\PairingService;
-
-use App\Application\Services\TaskService;
+use App\Application\Services\CaseService;
 use DI\ContainerBuilder;
 use function DI\autowire;
 use function DI\get;
 
 return function (ContainerBuilder $containerBuilder) {
     $containerBuilder->addDefinitions([
-        PairingService::class => autowire(PairingService::class),
-        TaskService::class => autowire(TaskService::class)
+        CaseService::class => autowire(CaseService::class)
     ]);
 };
