392c2af3912d35c2ea99d70f6a958a698c434633
Ivo Jansch <ivo@egeniq.com>
2020-10-19 01:28:29 +0200

    Added /case, / and /login default templates (with dummy data)

---
+++ portal/src/resources/views/login.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GGD BCO portaal - Login</title>
+
+    <!-- Stylesheet -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="{{ asset('js/app.js') }}"></script>
+</head>
+<body>
+
+<!-- Start of login component -->
+<div class="container-login">
+    <div class="card-login">
+        <img src="{{ asset('images/illustration-login.svg') }}" class="card-img-top" alt="Afbeelding digitaal contactonderzoek"/>
+
+        <div class="card-body">
+            <h1 class="card-title">Digitaal contactonderzoek</h1>
+        </div>
+
+        <div class="card-body">
+            <p class="card-text">...</p>
+        </div>
+
+        <div class="card-footer">
+            <a href="/" class="btn  btn-primary  btn-block">Inloggen met IdentityHub</a>
+        </div>
+    </div>
+</div>
+<!-- End of login component -->
+
+<!-- Bootstrap core JavaScript -->
+</body>
+</html>
