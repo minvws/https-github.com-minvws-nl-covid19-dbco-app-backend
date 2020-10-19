ce474e72bc20119aab0538d6293d6f87f3376fc0
Ivo Jansch <ivo@egeniq.com>
2020-10-19 16:35:10 +0200

    Initial version of identity hub login (and a dummy login provider for the time being)

---
+++ portal/src/app/Providers/Auth/IdentityHubProvider.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Providers\Auth;
+
+use Laravel\Socialite\Two\AbstractProvider;
+use Laravel\Socialite\Two\ProviderInterface;
+use Laravel\Socialite\Two\User;
+
+class IdentityHubProvider extends AbstractProvider implements ProviderInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase('https://login.ggdghor.nl/ggdghornl/oauth2/v1/auth', $state);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://login.ggdghor.nl/ggdghornl/oauth2/v1/token';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAccessToken($code)
+    {
+        $response = $this->getHttpClient()->post($this->getTokenUrl(), [
+            'headers' => ['Authorization' => 'Basic ' . base64_encode($this->clientId . ':' . $this->clientSecret)],
+            'body'    => $this->getTokenFields($code),
+        ]);
+
+        echo 'YEAH';
+        var_dump($response); die;
+
+        return $this->parseAccessToken($response->getBody());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenFields($code)
+    {
+        $fields = parent::getTokenFields($code);
+        $fields['grant_type'] = 'authorization_code';
+        return $fields;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get('https://login.ggdghor.nl/ggdghornl/oauth2/v1/userinfo', [
+            'headers' => [
+                'Authorization' => 'Bearer ' . $token,
+            ],
+        ]);
+
+        $user = json_decode($response->getBody(), true);
+        return $user;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User)->setRaw($user)->map([
+            'id'       => $user['unique_name'],
+            'nickname' => $user['nickname'],
+            'name'     => $user['name'],
+        ]);
+    }
+
+}
