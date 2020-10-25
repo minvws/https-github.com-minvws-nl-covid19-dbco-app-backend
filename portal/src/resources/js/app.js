42ee2f3eca9e622db080deb6fefdb1970feba9cb
Ivo Jansch <ivo@egeniq.com>
2020-10-25 10:54:54 +0100

    Clickable table rows and auto-inject new rows.

--- portal/src/resources/js/app.js
+++ portal/src/resources/js/app.js
@@ -1,1 +1,29 @@
 require('./bootstrap');
+
+jQuery(document).ready(function($) {
+    //  Make clickable rows in tables actually clickable
+    $(".clickable-row").click(function() {
+        window.location = $(this).data("href");
+    });
+
+    function cloneRow(el)
+    {
+        var tr = el.closest('tr');
+        var clone = tr.clone(true);
+        clone.insertAfter(tr).find('.auto-row-clone').one("keypress", function() {
+            cloneRow($(this));
+        });
+
+        clone.find('.form-control').each(function(i, input) {
+            // Bump input field names
+            input.name = input.name.replace(/\d+/, function(n) { return ++n });
+            input.value = '';
+        });
+    }
+
+    // Make auto row clone fields actually clone a row (upon the first keypress in the input field)
+    $(".auto-row-clone").one("keypress", function() {
+        cloneRow($(this));
+    });
+});
+
