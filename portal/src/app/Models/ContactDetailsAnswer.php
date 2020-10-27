dfad6b4038de1e04b944279e0d113f756740bb37
Ivo Jansch <ivo@egeniq.com>
2020-10-27 15:01:17 +0100

    Question/Answer models and repo's, and implementation of the robot friendly export screen.

---
+++ portal/src/app/Models/ContactDetailsAnswer.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+class ContactDetailsAnswer extends Answer
+{
+    public ?string $firstname;
+    public ?string $lastname;
+    public ?string $email;
+    public ?string $phonenumber;
+}
--- portal/src/app/Providers/RepositoryServiceProvider.php
+++ portal/src/app/Providers/RepositoryServiceProvider.php
@@ -2,14 +2,19 @@
 
 namespace App\Providers;
 
+use App\Repositories\AnswerRepository;
 use App\Repositories\ApiPairingRepository;
 use App\Repositories\CaseRepository;
+use App\Repositories\DbAnswerRepository;
 use App\Repositories\DbCaseRepository;
+use App\Repositories\DbQuestionnaireRepository;
+use App\Repositories\DbQuestionRepository;
 use App\Repositories\PairingRepository;
+use App\Repositories\QuestionnaireRepository;
+use App\Repositories\QuestionRepository;
 use App\Repositories\TaskRepository;
 use App\Repositories\DbTaskRepository;
 use Illuminate\Support\ServiceProvider;
-
 
 class RepositoryServiceProvider extends ServiceProvider
 {
@@ -23,6 +28,9 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(CaseRepository::class, DbCaseRepository::class);
         $this->app->bind(TaskRepository::class, DbTaskRepository::class);
         $this->app->bind(PairingRepository::class, ApiPairingRepository::class);
+        $this->app->bind(QuestionnaireRepository::class, DbQuestionnaireRepository::class);
+        $this->app->bind(AnswerRepository::class, DbAnswerRepository::class);
+        $this->app->bind(QuestionRepository::class, DbQuestionRepository::class);
     }
 
 }
