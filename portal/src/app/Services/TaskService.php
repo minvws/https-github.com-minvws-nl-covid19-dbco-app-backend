f9834859094ab4122a70739d12087a72b0b2e771
Ewout Pieter den Ouden <ewoutdenouden@gmail.com>
2020-11-04 14:28:17 +0100

    Store provided remote ID for task

--- portal/src/app/Services/TaskService.php
+++ portal/src/app/Services/TaskService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Models\Task;
 use App\Repositories\TaskRepository;
 
 class TaskService
@@ -10,5 +11,15 @@ class TaskService
 
     public function __construct(TaskRepository $taskRepository) {
         $this->taskRepository = $taskRepository;
+    }
+
+    public function getTask(string $taskUuid) {
+        return $this->taskRepository->getTask($taskUuid);
+    }
+
+    public function linkTaskToHpzone(Task $task, string $hpzoneId): void
+    {
+        $task->hpzoneId = $hpzoneId;
+        $this->taskRepository->updateTask($task);
     }
 }
