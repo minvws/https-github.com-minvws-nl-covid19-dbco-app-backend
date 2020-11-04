61aa703bbc5e651e6c94a8c359d4ed63d5a47fdd
Ewout Pieter den Ouden <ewoutdenouden@gmail.com>
2020-11-04 14:28:17 +0100

    Store provided remote ID for task

--- portal/src/app/Http/Controllers/TaskController.php
+++ portal/src/app/Http/Controllers/TaskController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Services\TaskService;
 use Illuminate\Http\Request;
 use App\Models\Task;
+use Illuminate\Http\Response;
 
 class TaskController extends Controller
 {
@@ -17,9 +18,20 @@ class TaskController extends Controller
 
     public function markUpload(Request $request)
     {
-        $taskId = $request->input('taskId');
-        $remoteId = $request->input('remoteId');
+        $remoteId = trim($request->input('remoteId'));
+        if (empty($remoteId)) {
+            return response()->json(['error' => "Hpzone ID is invalid"], Response::HTTP_BAD_REQUEST);
+        }
 
-        return response()->json(['success' => 'success'], 200);
+        $taskUuid = $request->input('taskId');
+        $task = $this->taskService->getTask($taskUuid);
+
+        if ($task === null) {
+            return response()->json(['error' => "Task $taskUuid is invalid"], Response::HTTP_BAD_REQUEST);
+        }
+
+        $this->taskService->linkTaskToHpzone($task, $remoteId);
+
+        return response()->json(['success' => 'success'], Response::HTTP_OK);
     }
 }
