dfb395037da7e860149e2c57a89f53fe12780a15
Ivo Jansch <ivo@egeniq.com>
2020-11-06 01:03:53 +0100

    Implemented asset versioning so we don't have caching issues

--- portal/src/webpack.mix.js
+++ portal/src/webpack.mix.js
@@ -14,4 +14,4 @@ const mix = require('laravel-mix');
 mix.js(['resources/js/app.js']
             , 'public/js')
     .sass('resources/scss/app.scss', 'public/css')
-    .sourceMaps();
+    .sourceMaps().version();
