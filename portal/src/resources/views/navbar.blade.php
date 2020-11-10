740314f7a39cb2b49addcc9ec3deefa9d7d98d2c
Ivo Jansch <ivo@egeniq.com>
2020-11-10 09:57:33 +0100

    Spurious whitespace

--- portal/src/resources/views/navbar.blade.php
+++ portal/src/resources/views/navbar.blade.php
@@ -22,7 +22,7 @@
                     <a class="nav-link" href="#">Omgeving: {{ ucfirst(App::environment()) }}</a>
                 </li>
                 @endif
-                <li class="nav-item ">
+                <li class="nav-item">
                     <a class="nav-link" href="#">eLearning</a>
                 </li>
                 <li class="nav-item">
