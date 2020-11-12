58ee45c5001129881f30fbb95269143fcecce109
Ewout Pieter den Ouden <ewoutdenouden@gmail.com>
2020-11-12 13:33:03 +0100

    WIP Implement questionnaire retrieval from database

--- api/src/healthauthority/bootstrap/repositories.php
+++ api/src/healthauthority/bootstrap/repositories.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 use DBCO\HealthAuthorityAPI\Application\Repositories\CaseRepository;
 use DBCO\HealthAuthorityAPI\Application\Repositories\DbCaseRepository;
+use DBCO\HealthAuthorityAPI\Application\Repositories\DbQuestionnaireRepository;
 use DBCO\HealthAuthorityAPI\Application\Repositories\StubCaseRepository;
 use DBCO\HealthAuthorityAPI\Application\Repositories\StubGeneralTaskRepository;
 use DBCO\HealthAuthorityAPI\Application\Repositories\StubQuestionnaireRepository;
@@ -15,7 +16,14 @@ use function DI\autowire;
 
 return function (ContainerBuilder $containerBuilder) {
     $containerBuilder->addDefinitions([
-        QuestionnaireRepository::class => autowire(StubQuestionnaireRepository::class),
+        QuestionnaireRepository::class => function (Container $c) {
+            $useStubs = $c->get('useStubs');
+            if ($useStubs) {
+                return $c->get(StubQuestionnaireRepository::class);
+            } else {
+                return $c->get(DbQuestionnaireRepository::class);
+            }
+        },
         GeneralTaskRepository::class => autowire(StubGeneralTaskRepository::class),
         CaseRepository::class => function (Container $c) {
             $useStubs = $c->get('useStubs');
