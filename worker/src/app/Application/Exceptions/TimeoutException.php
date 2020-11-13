39937a0e4118168039774ceed1f55fb5e4896759
Peter Verhage <peter@egeniq.com>
2020-11-13 09:55:13 +0100

    Improved pairing error forwarding

---
+++ worker/src/app/Application/Exceptions/TimeoutException.php
@@ -0,0 +1,11 @@
+<?php
+namespace DBCO\Worker\Application\Exceptions;
+
+use Exception;
+
+/**
+ * Thrown when a timeout occurs waiting for data.
+ */
+class TimeoutException extends Exception
+{
+}
