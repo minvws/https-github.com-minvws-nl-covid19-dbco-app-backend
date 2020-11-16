7617d656d96ccc4d97d92e60061eb50a88019872
Ewout Pieter den Ouden <ewoutdenouden@gmail.com>
2020-11-16 17:55:35 +0100

    Add other single-answer question types

--- api/src/healthauthority/tests/Application/Actions/QuestionnaireListActionTest.php
+++ api/src/healthauthority/tests/Application/Actions/QuestionnaireListActionTest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 namespace DBCO\HealthAuthorityAPI\Tests\Application\Actions;
 
 use DBCO\HealthAuthorityAPI\Application\Models\ClassificationDetailsQuestion;
+use DBCO\HealthAuthorityAPI\Application\Models\ContactDetailsQuestion;
+use DBCO\HealthAuthorityAPI\Application\Models\OpenQuestion;
 use DBCO\HealthAuthorityAPI\Application\Models\Question;
 use DBCO\HealthAuthorityAPI\Application\Models\Questionnaire;
 use DBCO\HealthAuthorityAPI\Application\Repositories\DbQuestionnaireRepository;
@@ -68,24 +70,34 @@ class QuestionnaireListActionTest extends TestCase
         $this->assertContainsEquals('00000000-0000-0000-0000-000000000002', $uuids);
     }
 
-    public function testListReturnsQuestions()
+    public function testQuestionnaireListContainsQuestions()
     {
         $data = $this->retrieveQuestionnaireList();
-        $this->assertObjectHasAttribute('questionnaires', $data);
+        $this->assertObjectHasAttribute('questionnaires', $data, 'No questionnaires returned');
 
+        // Retrieve the latest version of test Questionnare A
         $questionnaire = $this->findByUuid('00000000-0000-0000-0000-000000000001', $data->questionnaires);
-        $this->assertObjectHasAttribute('questions', $questionnaire);
-        $this->assertCount(1, $questionnaire->questions);
 
+        // Does the API call return the questions?
+        $this->assertObjectHasAttribute('questions', $questionnaire, 'Questionnaire does not contain any questions');
+        $this->assertCount(3, $questionnaire->questions, 'Incorrect number of questions returned for questionnaire');
+
+        // Check the first question to see if the data roundtrip works
         $question = $this->findByUuid('37d818ed-9499-4b9a-9771-725467368387', $questionnaire->questions);
+        $this->assertNotNull($question);
         $this->assertSame('classification', $question->group);
         $this->assertSame('Vragen over jullie ontmoeting', $question->label);
         $this->assertSame('Beschrijvingsveld', $question->description);
         $this->assertSame(Question::ALL_CATEGORIES, array_map(fn($q) => $q->category, $question->relevantForCategories));
+
+        // Check if the other questions are returned too
+        $this->assertNotNull($this->findByUuid('37d818ed-9499-4b9a-9770-725467368388', $questionnaire->questions));
+        $this->assertNotNull($this->findByUuid('37d818ed-9499-4b9a-9771-725467368389', $questionnaire->questions));
     }
 
     /**
-     * Helper method to locate an object.uuid in  an array of data from the API
+     * Helper method to locate a stdClass.uuid in an array of API-data
+     *
      * @param string $uuid
      * @param stdClass $jsonData
      * @return stdClass|null
@@ -139,6 +151,22 @@ class QuestionnaireListActionTest extends TestCase
         $question1->description = "Beschrijvingsveld";
         $question1->relevantForCategories = Question::ALL_CATEGORIES;
         $questionnaire->questions[] = $question1;
+
+        $question2 = new ContactDetailsQuestion();
+        $question2->uuid = "37d818ed-9499-4b9a-9770-725467368388";
+        $question2->group = "contactdetails";
+        $question2->label = "Contactgegevens";
+        $question2->description = null;
+        $question2->relevantForCategories = Question::ALL_CATEGORIES;;
+        $questionnaire->questions[] = $question2;
+
+        $question3 = new OpenQuestion();
+        $question3->uuid = "37d818ed-9499-4b9a-9771-725467368389";
+        $question3->group = "contactdetails";
+        $question3->label = "Beroep";
+        $question3->description = null;
+        $question3->relevantForCategories = ["1"];
+        $questionnaire->questions[] = $question3;
 
         $this->repository->storeQuestionnaire($questionnaire);
 
