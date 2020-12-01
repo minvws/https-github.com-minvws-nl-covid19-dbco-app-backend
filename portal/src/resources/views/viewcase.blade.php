3c30e674bb8c7e5cc0140455470f5de8660168bd
Ewout Pieter den Ouden <ewoutdenouden@gmail.com>
2020-12-01 12:13:13 +0100

    Add export button to portal case overview page

--- portal/src/resources/views/viewcase.blade.php
+++ portal/src/resources/views/viewcase.blade.php
@@ -26,9 +26,17 @@
                     @endif
                     @if ($case->isEditable())
                         <a class="btn btn-outline-primary" role="button" href="/editcase/{{ $case->uuid }}">Case wijzigen</a>
-                    @endif<a class="btn btn-primary" role="button" href="/dumpcase/{{ $case->uuid }}">Zet in HPZone</a>
+                    @endif
+                    <a class="btn btn-primary" role="button" href="/dumpcase/{{ $case->uuid }}">Zet in HPZone</a>
+                    <a class="btn btn-outline-primary" role="button" href="/exportcase/{{ $case->uuid }}">Klaarzetten voor index</a>
                 </span>
             </h2>
+            <!-- flash message -->
+            @if (session('message'))
+            <p>
+                <div class="alert">{!! session('message') !!}</div>
+            </p>
+            @endif
             <!-- End of page title component -->
             <p>
                 Casenr: {{ $case->caseId }}
