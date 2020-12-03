d6a851e5ed20c6d6906d4eab3752970fcb88b2bf
Peter Verhage <peter@egeniq.com>
2020-12-03 14:11:04 +0100

    HSM preparations

--- api/src/healthauthority/bootstrap/settings.php
+++ api/src/healthauthority/bootstrap/settings.php
@@ -2,6 +2,7 @@
 declare(strict_types=1);
 
 use Monolog\Logger;
+use Psr\Container\ContainerInterface;
 
 $debug = filter_var(getenv('DEBUG'), FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE) ?? false;
 
@@ -45,6 +46,9 @@ return [
     ],
     'privateAPI.jwtSecret' => DI\env('PRIVATE_API_JWT_SECRET'),
 
-    'signingKey.length' => 32,
-    'encryption.generalKeyPair' => DI\env('ENCRYPTION_GENERAL_KEY_PAIR')
+    'securityModule.type' => DI\env('SECURITY_MODULE_TYPE', 'hsm'),
+
+    // only used by the simple security module
+    'securityModule.skKeyExchange' => DI\env('SECURITY_MODULE_SK_KEY_EXCHANGE'),
+    'securityModule.skStore' => DI\env('SECURITY_MODULE_SK_STORE'),
 ];
