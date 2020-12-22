16ac43ccfc493e739186940de17e9685115d0928
Ewout Pieter den Ouden <ewoutdenouden@gmail.com>
2020-12-22 15:04:31 +0100

    Add last exposure date to sidebar to help debugging

--- portal/src/resources/views/taskquestionnaire.blade.php
+++ portal/src/resources/views/taskquestionnaire.blade.php
@@ -4,6 +4,21 @@
     </div>
 </div>
 
+<div class="row mt-3">
+    <div class="col">
+        <label for="date">
+            <strong>Laatste contact</strong>
+        </label>
+        <input type="date"
+               maxlength="255"
+               class="form-control"
+               id="lastcontactdate"
+               name="lastcontactdate"
+               value="{{ isset($task['dateOfLastExposure']) ? $task['dateOfLastExposure']->format('Y-m-d') : ''}}"
+               placeholder="" />
+    </div>
+</div>
+
 @foreach($questions as $question)
     @if (in_array($task['category'], $question->relevantForCategories))
         @include("taskquestion_".$question->questionType)
