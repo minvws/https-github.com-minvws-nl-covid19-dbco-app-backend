059bb2509926a538e0642fac5f0f37eeb342ccd1
Ivo Jansch <ivo@egeniq.com>
2021-01-03 22:15:16 +0100

    Removed sample component

--- portal/src/resources/js/app.js
+++ portal/src/resources/js/app.js
@@ -24,7 +24,6 @@ Vue.use(BootstrapVue);
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('covid-case-table-component', require('./components/CovidCaseTableComponent').default);
 
 /**
