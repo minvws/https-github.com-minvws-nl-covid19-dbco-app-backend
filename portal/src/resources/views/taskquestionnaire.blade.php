5874b06ef5a23066131c453e1a5ce85aa8ad2896
Ewout Pieter den Ouden <ewoutdenouden@gmail.com>
2021-01-04 12:22:11 +0100

    DBCO-429: MVP add last contact date to sidebar

--- portal/src/resources/views/taskquestionnaire.blade.php
+++ portal/src/resources/views/taskquestionnaire.blade.php
@@ -4,8 +4,22 @@
     </div>
 </div>
 
-@foreach($questions as $question)
-    @if (in_array($task['category'], $question->relevantForCategories))
-        @include("taskquestion_".$question->questionType)
-    @endif
-@endforeach
+<form method="post"
+      action="{{ route('task-questionnaire-save', [$task['uuid']]) }}">
+@csrf
+
+    <!-- Questionnaire -->
+    @foreach($questions as $question)
+        @if (in_array($task['category'], $question->relevantForCategories))
+            @include("taskquestion_".$question->questionType)
+        @endif
+    @endforeach
+    <!-- End of questionnaire -->
+
+    <!-- Form submit -->
+    <div class="btn-group mb-3 mt-3">
+        <input type="submit" class="btn btn-primary" value="Opslaan" />
+    </div>
+    <!-- End of form submit-->
+
+</form>
