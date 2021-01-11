cd3f4cfc30758f48c501ec6ed85a227f0331c07c
Maurice Maas <maurice@egeniq.com>
2021-01-11 11:22:24 +0100

    DBCO-579: topbar styling on vue branch

--- portal/src/resources/views/navbar.blade.php
+++ portal/src/resources/views/navbar.blade.php
@@ -1,7 +1,7 @@
 <!-- Start of navbar component -->
 <div>
-<b-navbar type="dark" fixed="top" variant="dark">
-    <b-navbar-brand href="#"><</b-navbar-brand>
+<b-navbar toggleable="lg" type="dark" fixed="top" class="navbar-custom">
+    <b-navbar-brand href="#"><i class="icon icon--breadcrumb-back"></i>Mijn cases</b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
@@ -10,8 +10,9 @@
             @if (App::environment() != 'production')
                 <b-nav-item>Omgeving: {{ ucfirst(App::environment()) }}</b-nav-item>
             @endif
-            <b-nav-item href="#">eLearning</b-nav-item>
-            <b-nav-item href="#">Helpdesk</b-nav-item>
+            <b-nav-item href="#"><i class="icon icon--feedback"></i>Feedback</b-nav-item>
+            <b-nav-item href="#"><i class="icon icon--helpdesk"></i>Helpdesk</b-nav-item>
+            <b-nav-item href="#"><i class="icon icon--support"></i>Support</b-nav-item>
             <b-nav-item href="{{ route('user-profile')}}">{{ $userName }}</b-nav-item>
         </b-navbar-nav>
     </b-collapse>
